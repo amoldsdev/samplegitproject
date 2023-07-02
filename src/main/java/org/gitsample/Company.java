74c86975bc06d6d275c055289f2c853f3bc312ee
amoldsdev <111508537+amoldsdev@users.noreply.github.com>
2023-07-02 19:21:12 +0530

    Add files via upload

---
+++ src/main/java/org/gitsample/Company.java
@@ -0,0 +1,28 @@
+package org.gitsample;
+
+public class Company {
+	
+
+	//Change 2
+
+	//Change 1
+
+	private String name;
+	private String city;
+	//commit1
+	private int salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return city;
+	}
+	public void setAddress(String address) {
+		this.city = city;
+	}
+	//Just a comment!
+	
+}
